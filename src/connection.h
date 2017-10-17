0287c4038f2d8003aebb0387bd7e77a4703ae856
kongxun <idbord@qq.com>
2017-10-17 23:21:57 +0800

    optimize

--- src/connection.h
+++ src/connection.h
@@ -6,17 +6,43 @@
 class Connection
 {
 public:
-    Connection(std::string, std::string);
+    Connection(std::string url, std::string filepath):url(url), filepath(filepath){}
 
-    void set_start_byte(int start_byte);
-    void set_conn_size(int conn_size);
-    void set_conn_id(int conn_id);
-    int get_start_byte();
-    int get_end_byte();
-    int get_conn_size();
-    int get_conn_id();
-    std::string get_url();
-    std::string get_file_path();
+    void set_start_byte(int start_byte) {
+        this->start_byte = start_byte;
+    }
+
+    void set_conn_size(int conn_size) {
+        this->conn_size = conn_size;
+    }
+
+    void set_conn_id(int conn_id) {
+        this->conn_id = conn_id;
+    }
+
+    int get_start_byte() {
+        return this->start_byte;
+    }
+
+    int get_end_byte() {
+        return this->start_byte + this->conn_size -1;
+    }
+
+    int get_conn_size() {
+        return this->conn_size;
+    }
+
+    std::string get_url() {
+        return this->url;
+    }
+
+    int get_conn_id() {
+        return this->conn_id;
+    }
+
+    std::string get_file_path() {
+        return this->filepath;
+    }
 
 private:
     int start_byte=0;
--- src/util.h
+++ src/util.h
@@ -19,25 +19,64 @@
 class Util
 {
 public:
-    Util();
+    Util(){}
 
-    static int get_cpu_core_num();
+    /**
+     * @brief get_cpu_core_num
+     * @return
+     */
+    static int get_cpu_core_num() {
+        #if _WIN32
+            SYSTEM_INFO info;
+            GetSystemInfo(&info);
+            return info.dwNumberOfProcessors;
+        #elif __APPLE__ || linux || __unix__
+            return get_nprocs();
+        #else
+            return 4;
+        #endif
+    }
 
     /**
      * @brief isDirExist 判断输出目录是否存在
      * @return
      */
-    static int is_dir_exist(std::string& dir);
+    static int is_dir_exist(std::string& dir) {
+        #if _WIN32
+            struct _stat fileStat;
+            if ((_stat(dir.c_str(), &fileStat) == 0) && (fileStat.st_mode & _S_IFDIR))
+            {
+                return 0;
+        #else
+            struct stat fileStat;
+            if ((stat(dir.c_str(), &fileStat) == 0) && S_ISDIR(fileStat.st_mode))
+            {
+                return 0;
+        #endif
+            }else{
+                return -1;
+            }
+    }
 
     /**
      * @brief writelog
      * @param str
      */
-    static void writelog_(const char* str, const char* file, int line);
+    static void writelog_(const char* str, const char* file, int line) {
+        std::cout<<"[file]:"<<file<<"\t"<<"[line]:"<<line<<"\t"<<"[msg]:"<<str<<std::endl;
+    }
 
-    static void str_append(std::string &str, const int num);
+    static void str_append(std::string &str, const int num) {
+        std::stringstream ss;
+        ss<<num;
+        str.append(ss.str());
+    }
 
-    static void str_append(std::string &str, const char* chs);
+    static void str_append(std::string &str, const char* chs) {
+        std::stringstream ss;
+        ss<<chs;
+        str.append(ss.str());
+    }
 };
 
 #endif // UTIL_H
